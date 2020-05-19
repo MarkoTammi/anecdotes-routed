ec211af5949719d3d72081054c3d0754453e6c77
Tammivuori <marko.tammivuori@gmail.com>
2020-05-19 13:05:13 +0300

    7.1: routed anecdotes, step1

---
+++ src/components/Menu.js
@@ -0,0 +1,31 @@
+
+
+
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch, Route, Link
+} from "react-router-dom"
+
+const Menu = () => {
+    const padding = {
+      paddingRight: 5
+    }
+    return (
+      <div>
+
+{/*     //Original stuff - to be removed
+        <a href='#' style={padding}>anecdotes</a>
+        <a href='#' style={padding}>create new</a>
+        <a href='#' style={padding}>about</a> */}
+
+        <Link style={padding} to="/">anecdotes</Link>
+        <Link style={padding} to="/create">create new</Link>
+        <Link style={padding} to="/about">about</Link>
+
+
+      </div>
+    )
+  }
+
+export default Menu
